c3a39ba8c6689d8c4d18e5dcf9f6c428a4296bb0
HyRALis <petar.trajanoski.pt@gmail.com>
2022-03-30 01:51:33 +0200

    Changed the class into className

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -19,24 +19,24 @@ export default function Navbar() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarText">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="#">
                                 Home
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
                                 Features
                             </a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
                                 Pricing
                             </a>
                         </li>
                     </ul>
-                    <span class="navbar-text">Navbar text with an inline element</span>
+                    <span className="navbar-text">Navbar text with an inline element</span>
                 </div>
             </div>
         </nav>
