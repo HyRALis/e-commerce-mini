a5ac763b578ea91e812a3f8b5baa8b44677878e7
HyRALis <petar.trajanoski.pt@gmail.com>
2022-03-30 01:53:37 +0200

    Implemented the product cards in the Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import styles from '../styles/Pages/Home.module.scss';
-import Container from '../UI layer/Container';
 
-export default function Home() {
+import Container from '../UI layer/Container';
+import Card from '../UI layer/Card';
+
+import styles from '../styles/Pages/Home.module.scss';
+
+export default function Home({ products, states, categories }) {
+    const productCards = products.map((product) => (
+        <div className="col">
+            <Card key={`product_card_${product.id}`} product={product} />
+        </div>
+    ));
+
     return (
-        <main>
+        <>
             <img
                 className={styles.home__heroImage}
                 src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
                 alt="Hero"
             />
-            <Container></Container>
-        </main>
+            <Container>
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3">{productCards}</div>
+            </Container>
+        </>
     );
 }
