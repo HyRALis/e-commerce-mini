838aafcd2ef419fc049d1da96845cf9e64007119
Petar Trajanoski <petar.trajanoski.pt@gmail.com>
2022-04-02 21:54:15 +0200

    Added the Dropdowns and the Add products button

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Container from '../UI layer/Container';
 import Card from '../UI layer/Card';
 import Dropdown from '../UI layer/Dropdown';
 
 import styles from '../styles/Pages/Home.module.scss';
+import Plus from '../UI layer/svg/Plus';
 
 export default function Home({ products, states, categories }) {
+    const [selectedSortingState, setSelectedSortingState] = useState(null);
+    const [selectedSortingCategory, setSelectedSortingCategory] = useState(null);
+
     const productCards = products.map((product) => (
         <div className="col">
             <Card key={`product_card_${product.id}`} product={product} />
@@ -20,10 +24,24 @@ export default function Home({ products, states, categories }) {
                 src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
                 alt="Hero"
             />
-            <Container>
-                <div className="">
-                    <Dropdown options={states} />
-                    <Dropdown options={categories} />
+            <Container classes={'position-relative'}>
+                <div className={`${styles.actions__container} ml-2`}>
+                    <div className={styles.sorting__container}>
+                        <Dropdown
+                            options={states}
+                            placeholder="Sort by state"
+                            onSelectChnage={setSelectedSortingState}
+                        />
+                        <Dropdown
+                            options={categories}
+                            placeholder="Sort by category"
+                            onSelectChnage={setSelectedSortingCategory}
+                        />
+                    </div>
+                    <button className={styles.custom__button}>
+                        Add Product
+                        <Plus fill="#ffffff" width="0.8rem" height="0.8rem" className="ml-2" />
+                    </button>
                 </div>
                 <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3">{productCards}</div>
             </Container>
