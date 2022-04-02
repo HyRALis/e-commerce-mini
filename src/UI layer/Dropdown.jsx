1ae3af9db14fe400e63f34a0ea92ddcc9d1bb9ef
Petar Trajanoski <petar.trajanoski.pt@gmail.com>
2022-04-02 21:51:46 +0200

    Finished the dropdown styling and implementation

--- src/UI layer/Dropdown.jsx
+++ src/UI layer/Dropdown.jsx
@@ -1,42 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+
+import { useOnClickOutside } from '../Utils/customHooks';
 
 import styles from '../styles/UI layer/Dropdown.module.scss';
+import ArrowDown from './svg/ArrowDown';
 
-export default function Dropdown({ options }) {
+export default function Dropdown({ options, placeholder, onSelectChnage }) {
     const [isOpen, setIsOpen] = useState(false);
     const [selectedOption, setSelectedOption] = useState(null);
 
+    const dropdownRef = useRef(null);
+
     const toggling = () => setIsOpen(!isOpen);
 
-    const onOptionClicked = (value) => () => {
-        setSelectedOption(value);
+    const onOptionClicked = (option) => () => {
+        setSelectedOption(option.name);
         setIsOpen(false);
-        console.log(selectedOption);
+        onSelectChnage(option);
     };
 
+    useOnClickOutside(dropdownRef, () => setIsOpen(false));
+
     return (
-        <div className={styles.main}>
-            <h1>Custom Select/dropdown</h1>
-            <div className={styles.dropdown__container}>
-                <div className={styles.dropdown__container} onClick={toggling}>
-                    {selectedOption || 'Mangoes'}
-                </div>
-                {isOpen && (
-                    <div>
-                        <ul className={styles.dropdown__list}>
-                            {options.map((option) => (
-                                <li
-                                    className={styles.dropdown__list__item}
-                                    onClick={onOptionClicked(option)}
-                                    key={Math.random()}
-                                >
-                                    {option.name}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                )}
+        <div ref={dropdownRef} className={styles.dropdown__container}>
+            <div className={styles.dropdown__header} aria-haspopup="listbox" aria-expanded={isOpen} onClick={toggling}>
+                <span>{selectedOption || placeholder}</span>
+                <ArrowDown fill="#3faffa" width="0.8rem" height="0.8rem" className={styles.rotatesvg} />
             </div>
+            {isOpen && (
+                <ul className={styles.dropdown__list} role="listbox" tabIndex={-1}>
+                    {options.map((option) => (
+                        <li
+                            key={option.id}
+                            className={styles.dropdown__list__item}
+                            onClick={onOptionClicked(option)}
+                            tabIndex={0}
+                        >
+                            {option.name}
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 }
