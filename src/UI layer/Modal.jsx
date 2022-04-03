6d3e75b125112ba49bdaee84390eb74bca7291e1
Petar Trajanoski <petar.trajanoski.pt@gmail.com>
2022-04-03 18:55:52 +0200

    fix for click inside closing the modal

--- src/UI layer/Modal.jsx
+++ src/UI layer/Modal.jsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
+
+import { useOnClickOutside } from '../Utils/customHooks';
+
 import styles from '../styles/UI layer/Modal.module.scss';
 
 function Modal({ setIsModalOpen, title }) {
+    const modalContainerRef = useRef(null);
+
+    useOnClickOutside(modalContainerRef, () => setIsModalOpen(false));
+
     return (
-        <div
-            className={styles.modalBackground}
-            onClick={() => {
-                setIsModalOpen(false);
-            }}
-        >
-            <div className={styles.modalContainer}>
+        <div className={styles.modalBackground}>
+            <div ref={modalContainerRef} className={styles.modalContainer}>
                 <div className={styles.titleCloseBtn}>
                     <button
                         onClick={() => {
