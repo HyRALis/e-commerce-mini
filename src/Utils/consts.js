a19b72a831d2991d2890c61f33fff930993bf7fe
Petar Trajanoski <petar.trajanoski.pt@gmail.com>
2022-04-04 05:14:00 +0200

    Added consts for validation types

---
+++ src/Utils/consts.js
@@ -0,0 +1,7 @@
+// VALIDATION TUPES
+
+export const REQUIRED = 'required';
+export const ALLOWED_VALUE = 'allowedValue';
+export const MIN_CHARS = 'minChars';
+export const MIN_VALUE = 'minValue';
+export const VALID_URL = 'isUrl';
