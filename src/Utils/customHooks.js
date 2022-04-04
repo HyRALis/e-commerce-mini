f6a1dd994774355d523ea1149c3eda4ee4bc5661
Petar Trajanoski <petar.trajanoski.pt@gmail.com>
2022-04-04 05:15:04 +0200

    Added custom hooks for: useOnUpdated & useValidateField

--- src/Utils/customHooks.js
+++ src/Utils/customHooks.js
@@ -1,4 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+
+import { isValidValue } from './validation';
+
+export const useOnUpdate = (callback, dependencies) => {
+    const isInitialMount = useRef(true);
+
+    useEffect(() => {
+        if (isInitialMount.current) {
+            isInitialMount.current = false;
+        } else {
+            callback();
+        }
+    }, dependencies);
+};
 
 export const useOnClickOutside = (ref, handler) => {
     useEffect(
@@ -28,4 +42,30 @@ export const useOnClickOutside = (ref, handler) => {
         // ... passing it into this hook.
         [ref, handler]
     );
+};
+
+export const useValidateField = (field, fieldName, setError, errors) => {
+    const validateField = (field) => {
+        if (!field.validationTypes) return true;
+
+        let isValid = true;
+        let errorMessage = '';
+        for (let index = 0; index < field.validationTypes.length; index++) {
+            if (!isValid) break;
+            const validCheck = isValidValue(field, field.validationTypes[index]);
+            isValid = validCheck.isValid;
+            errorMessage = validCheck.errorMessage;
+        }
+
+        return { isValid, errorMessage };
+    };
+
+    useOnUpdate(() => {
+        const { isValid, errorMessage } = validateField(field);
+        if (!isValid) {
+            setError({ ...errors, [fieldName]: errorMessage });
+        } else {
+            setError({ ...errors, [fieldName]: '' });
+        }
+    }, [field]);
 };
