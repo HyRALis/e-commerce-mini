e55926196ecc799d2d476de00166f76cc3202f84
HyRALis <petar.trajanoski.pt@gmail.com>
2022-04-06 12:06:23 +0200

    removed exact

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <Router>
             <Navbar />
             <Routes>
-                <Route exact path="/" element={<Home />} />
+                <Route path="/" element={<Home />} />
             </Routes>
         </Router>
     );
