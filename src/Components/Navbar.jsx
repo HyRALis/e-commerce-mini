bc12e52f40aadc7b8c8acc2716e382b0495f78cd
HyRALis <petar.trajanoski.pt@gmail.com>
2022-04-06 10:41:46 +0200

    Removed unnecessary code

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,26 +18,6 @@ export default function Navbar() {
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarText">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">
-                                Home
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                Features
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                Pricing
-                            </a>
-                        </li>
-                    </ul>
-                    <span className="navbar-text">Navbar text with an inline element</span>
-                </div>
             </div>
         </nav>
     );
