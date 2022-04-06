9d29000464c05cbb5d8ab0839ed303f99740fe7a
HyRALis <petar.trajanoski.pt@gmail.com>
2022-04-06 02:10:32 +0200

    Added sortingOptions

--- src/Utils/consts.js
+++ src/Utils/consts.js
@@ -1,7 +1,18 @@
 // VALIDATION TUPES
-
 export const REQUIRED = 'required';
 export const ALLOWED_VALUE = 'allowedValue';
 export const MIN_CHARS = 'minChars';
 export const MIN_VALUE = 'minValue';
 export const VALID_URL = 'isUrl';
+
+// SORTING OPTIONS
+export const sortingOptions = [
+    {
+        id: 1,
+        name: 'Ascending'
+    },
+    {
+        id: -1,
+        name: 'Descending'
+    }
+];
